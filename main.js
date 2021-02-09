4cd7a5f94963fc08c2c0beef777f2143cda95de2
Jenderson Quintero <j.quintero@correo.unimet.edu.ve>
2021-02-09 17:08:44 -0400

    Creacion del carrusel (No funcional)

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+/* Función para mover el carrusel */
+
 addEventListener("DOMContentLoaded", () => {
-    const imagenes = ["img/fondocarrusel1", "img/fondocarrusel2", "img/fondocarrusel3", "img/fondocarrusel4"]
+
+    const imagenes = ["img/fondocarrusel1.jpg", "img/fondocarrusel2.jpg", "img/fondocarrusel3.jpg", "img/fondocarrusel4.jpg"]
     let i = 1
     const img1 = document.querySelector("#img1")
     const img1 = document.querySelector("#img2")
@@ -9,10 +12,25 @@ addEventListener("DOMContentLoaded", () => {
         div.id = index
         
     }
+
+    img1.src = imagenes[0]
+
+    const carrusel = () => {
+       img2.src = imagenes[i]
+       img2.classList.add("active")
+       i++
+
+       if (i == imagenes.length) {
+           i = 0
+        }
+
+       setTimeout(() => {
+           img1.src = img2.src
+           img2.classList.remove("active")
+        }, 1000)
+    }
+
+    setInterval(carrusel, 4000)
 })
-
-
-
-
 
 
