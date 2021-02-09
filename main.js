e3631a5b86b5400b4cb5951d474c1ebaeec972af
Jenderson Quintero <j.quintero@correo.unimet.edu.ve>
2021-02-09 16:35:55 -0400

    Creación del Hero

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+addEventListener("DOMContentLoaded", () => {
+    const imagenes = ["img/fondocarrusel1", "img/fondocarrusel2", "img/fondocarrusel3", "img/fondocarrusel4"]
+    let i = 1
+    const img1 = document.querySelector("#img1")
+    const img1 = document.querySelector("#img2")
+
+    for (let indez = 0; index < imagenes.length; index++) {
+        const div = document.createElement("div")
+        div.id = index
+        
+    }
+})
+
+
+
+
+
 
